a8d5b0a5c69c7e1861dacf086cf07ba34c47b7d2
Rajarshi Roy <rajarshiroy2005456@gmail.com>
2026-04-10 22:16:09 +0530

    auth fix

--- chat/src/context/AuthContext.jsx
+++ chat/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { loginRequest, signupRequest } from "../services/AuthService";
+import { loginRequest, signupRequest } from "../services/AuthServices";
 
 const AuthContext = createContext();
 
